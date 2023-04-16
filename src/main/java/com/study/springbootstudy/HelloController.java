4b752cdedd4c8d141faca0b6d3c536f4823ae4b2
changerlemond <83348294+changerlemond@users.noreply.github.com>
2023-04-16 19:52:41 +0900

    http request, response test

--- src/main/java/com/study/springbootstudy/HelloController.java
+++ src/main/java/com/study/springbootstudy/HelloController.java
@@ -1,2 +1,14 @@
-package com.study.springbootstudy;public class HelloController {
+package com.study.springbootstudy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+
 }
