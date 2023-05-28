01aa46ce5812cd478432e7d4fb660b4ca37fc48c
changerlemond <83348294+changerlemond@users.noreply.github.com>
2023-05-28 17:30:37 +0900

    servlet test

--- src/main/java/com/study/springbootstudy/SpringBootStudyApplication.java
+++ src/main/java/com/study/springbootstudy/SpringBootStudyApplication.java
@@ -1,13 +1,33 @@
 package com.study.springbootstudy;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
 public class SpringBootStudyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootStudyApplication.class, args);
+        ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> servletContext.addServlet("hello", new HttpServlet() {
+            @Override
+            protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+                String name = req.getParameter("name");
+
+                resp.setStatus(HttpStatus.OK.value());
+                resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                resp.getWriter().println("Hello " + name);
+            }
+        }).addMapping("/hello"));
+        webServer.start();
     }
 
 }
