5e7b691814edfd70ccc262a7721c124e3401664a
changerlemond <83348294+changerlemond@users.noreply.github.com>
2023-05-28 19:53:28 +0900

    junit test code

--- src/main/java/com/study/springbootstudy/HelloController.java
+++ src/main/java/com/study/springbootstudy/HelloController.java
@@ -1,27 +1,22 @@
 package com.study.springbootstudy;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Objects;
 
 @RestController
 public class HelloController {
 
     private final HelloService simpleHelloService;
 
-    private final ApplicationContext applicationContext;
-
-    public HelloController(HelloService simpleHelloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService simpleHelloService) {
         this.simpleHelloService = simpleHelloService;
-        this.applicationContext = applicationContext;
-
-        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
-        return simpleHelloService.sayHello(Objects.requireNonNull(name));
+        if (name == null || name.trim().length() == 0) {
+            throw new IllegalArgumentException();
+        }
+        return simpleHelloService.sayHello(name);
     }
 }
--- src/test/java/com/study/springbootstudy/HelloApiTest.java
+++ src/test/java/com/study/springbootstudy/HelloApiTest.java
@@ -20,4 +20,13 @@ public class HelloApiTest {
         assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         assertThat(response.getBody()).isEqualTo("Hello Spring");
     }
+
+    @Test
+    void failHelloApi() {
+        TestRestTemplate testTemplate = new TestRestTemplate();
+
+        ResponseEntity<String> response =
+                testTemplate.getForEntity("http://localhost:8080/hello?name=", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
