fd9a0778db3c553113517c6174588c032dba433c
changerlemond <83348294+changerlemond@users.noreply.github.com>
2023-05-28 18:38:57 +0900

    dispatcher servlet test

--- src/main/java/com/study/springbootstudy/HelloController.java
+++ src/main/java/com/study/springbootstudy/HelloController.java
@@ -1,10 +1,24 @@
 package com.study.springbootstudy;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
+
+    private final HelloService simpleHelloService;
+
+    public HelloController(HelloService simpleHelloService) {
+        this.simpleHelloService = simpleHelloService;
+    }
+
+    @GetMapping
+    @ResponseBody
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(String name) {
-        SimpleHelloService simpleHelloService = new SimpleHelloService();
         return simpleHelloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/com/study/springbootstudy/HelloService.java
@@ -0,0 +1,5 @@
+package com.study.springbootstudy;
+
+public interface HelloService {
+    String sayHello(String name);
+}
