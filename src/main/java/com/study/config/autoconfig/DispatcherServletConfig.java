44e0d5fa03e3d2071545ed27ceff18b0e70a533c
changerlemond <83348294+changerlemond@users.noreply.github.com>
2023-05-29 19:44:12 +0900

    auto configuration annotation test

--- src/main/java/com/study/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/com/study/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package com.study.config.autoconfig;
 
+import com.study.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
     @Bean
