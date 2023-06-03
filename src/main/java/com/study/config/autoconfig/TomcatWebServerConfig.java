a0c5a5a0c365d64bc2f6c08263c10bed0586921d
changerlemond <83348294+changerlemond@users.noreply.github.com>
2023-06-03 16:56:34 +0900

    custom conditional study

--- src/main/java/com/study/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/study/config/autoconfig/TomcatWebServerConfig.java
@@ -1,26 +1,17 @@
 package com.study.config.autoconfig;
 
+import com.study.config.ConditionalMyOnClass;
 import com.study.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
     @Bean("tomcatServletWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
-    }
-
-    static class TomcatCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, org.springframework.core.type.AnnotatedTypeMetadata metadata) {
-            return false;
-        }
     }
 }
